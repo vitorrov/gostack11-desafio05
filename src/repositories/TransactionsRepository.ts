8c9c76b681a979ef3edbacbfcce717f3c7567f75
Vitor <vitormr45@gmail.com>
2020-04-21 17:29:11 -0300

    Finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,36 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let outcome = 0;
+    let income = 0;
+
+    const total = this.transactions.reduce((totalValue, currentValue) => {
+      if (currentValue.type === 'income') {
+        income += currentValue.value;
+        return totalValue + currentValue.value;
+      }
+
+      outcome += currentValue.value;
+      return totalValue - currentValue.value;
+    }, 0);
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
